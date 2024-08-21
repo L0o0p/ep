4abcb50261e0468b343b276696ec620e1ee0a8ff
lux <lux@luxhub.top>
2024-08-21 14:53:03 +0800

    change build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from "path";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './', // 使用相对路径
+//   build: {
+//     outDir: 'build'
+//   },
+// })
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: './', // 使用相对路径
   build: {
     outDir: 'build'
   },
-})
+  plugins: [react({})],
+  server: {
+    watch: {
+      ignored: ["!**/node_modules/**"],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
+      "~": resolve(__dirname, "src"),
+    },
+  },
+});
