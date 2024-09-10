4c9755d2d927c7d832898693f642c6f882531e61
lux <lux@luxhub.top>
2024-09-11 01:23:34 +0800

    1

--- src/ollama.tsx
+++ src/ollama.tsx
@@ -1,7 +1,7 @@
 import ollama from 'ollama';
 
 
-(ollama as any).config.host = 'https://bjol.luxhub.top:9445/a/';
+(ollama as any).config.host = 'https://bjol.luxhub.top:9445/b/';
 const premise = `对于你的返回内容，有以下几点要求：
 1. 针对我发给你的图片，返回对图片内容和图片风格的描述标签，比如：“女孩”，“街景”，“赛博朋克风格”，不需要任何其他返回信息。
 2. 针对多次对话中的相似内容或风格返回的词条应当相对固定，比如对于赛博朋克风格应该返回完全相同的词条“赛博朋克风格”，避免一次使用”赛博朋克“，另一次使用“赛博朋克风格”的情况`
